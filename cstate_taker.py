4bd037bc80320348dc416cddccebd4fa0f289b79
MalvaTalpa <ciaosara15@yahoo.com>
2024-12-04 21:08:54 +0100

    added both CPU divided and State

--- cstate_taker.py
+++ cstate_taker.py
@@ -10,7 +10,30 @@ import pandas as pd
 import json
 
 
-def json_to_excel(type, counter_type):
+def json_to_excel_states(type, counter_type):
+    timestamps = []
+    cpus = []
+    states = []
+
+    for i in range(len(counter_type)):
+        timestamps.append(counter_type[i]["timestamp"])
+
+    for i in range(len(counter_type[0]["block"])):
+        cpus.append(counter_type[0]["block"][i]["cpu"])
+
+    for i in range(len(counter_type[0]["block"][0]["state"])):
+        states.append(counter_type[0]["block"][0]["state"][i]["state"])
+
+    df = pd.DataFrame(columns=cpus, index=timestamps)
+    for j in range(len(states)):
+        for i in range(len(timestamps)):
+            for k in range(len(cpus)):
+                a = counter_type[i]["block"][k]["state"][j]["value"]
+                df.at[timestamps[i], cpus[k]] = int(a.rstrip('\n'))
+            filename = "/home/ubuntu/Get_time_usage_irl/results_state/" + type + "_" + states[j] + ".csv"
+        df.to_csv(filename)
+
+def json_to_excel_cpus(type, counter_type):
     timestamps = []
     cpus = []
     states = []
@@ -31,8 +54,9 @@ def json_to_excel(type, counter_type):
                 a = counter_type[i]["block"][j]["state"][k]["value"]
                 a.rstrip('\n')
                 df.at[timestamps[i], states[k]] = int(a.rstrip('\n'))
-            filename = "/home/ubuntu/Get_time_usage_irl/results/" + type + "_" + cpus[j] + ".csv"
+            filename = "/home/ubuntu/Get_time_usage_irl/results_cpus/" + type + "_" + cpus[j] + ".csv"
         df.to_csv(filename)
+
 
 
 
@@ -72,12 +96,14 @@ for i in range(maxtime):
         temp_time.block.append(cpu_time)
         temp_usage.block.append(cpu_usage)
 
-    counters_time.append(temp_time.model_dump(mode='json'))
-    counters_usage.append(temp_usage.model_dump(mode='json'))
+    counters_time.append(temp_time.model_dump(mode='json',context=OrderedDict))
+    counters_usage.append(temp_usage.model_dump(mode='json', context=OrderedDict))
     i += tstep
 
 
-json_to_excel("time", counters_time)
-json_to_excel("usage", counters_usage)
+json_to_excel_cpus("time", counters_time)
+json_to_excel_cpus("usage", counters_usage)
+json_to_excel_states("time", counters_time)
+json_to_excel_states("usage", counters_usage)
 print("dumps")
 
